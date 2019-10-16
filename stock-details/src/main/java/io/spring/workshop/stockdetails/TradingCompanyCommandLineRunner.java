0c420ad91f0b69d90130418a1bd85b73178adb29
LM\N0269887 <glen.miskimmons@liberty-it.co.uk>
2019-10-16 12:01:32 -0400

    Stock Details Application
    Use a reactive datastore

---
+++ stock-details/src/main/java/io/spring/workshop/stockdetails/TradingCompanyCommandLineRunner.java
@@ -0,0 +1,27 @@
+package io.spring.workshop.stockdetails;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@Component
+public class TradingCompanyCommandLineRunner implements CommandLineRunner {
+    private TradingCompanyRepository repository;
+
+    TradingCompanyCommandLineRunner(TradingCompanyRepository repository) {
+        this.repository = repository;
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        Flux.just(new TradingCompany("Pivotal Software", "PVTL"),
+                new TradingCompany("Dell Technologies", "DELL"),
+                new TradingCompany("Google", "GOOG"),
+                new TradingCompany("Microsoft", "MSFT"),
+                new TradingCompany("Oracle", "ORCL"),
+                new TradingCompany("Red Hat", "RHT"),
+                new TradingCompany("Vmware", "VMW"))
+                .flatMap(repository::save)
+                .thenMany(repository.findAll())
+                .subscribe(System.out::println);
+    }
+}
